688be5a6dd5b0b372f71b471b9ae42a953451291
jbq428 <jbq428@gmail.com>
2018-10-27 13:32:16 -0400

    working commit

--- AndroidStudioProjects/FridgyProject/app/src/main/java/com/example/jbq42/fridgyproject/Main2Activity.java
+++ AndroidStudioProjects/FridgyProject/app/src/main/java/com/example/jbq42/fridgyproject/Main2Activity.java
@@ -9,5 +9,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
     }
 }
