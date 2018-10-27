c7f7049773338f8a724355c23d419f3ed7219bf0
jbq428 <jbq428@gmail.com>
2018-10-27 12:11:15 -0400

    test commit

---
+++ AndroidStudioProjects/FridgyProject/app/src/main/java/com/example/jbq42/fridgyproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jbq42.fridgyproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button pressBtn = (Button) findViewById(R.id.pressBtn);
+
+
+    }
+}
